57005dd884fcdeef076f7ec7f7e4879f9414c3d2
ArumDwiCahyani <134916757+ArumDwiCahyani@users.noreply.github.com>
2023-05-29 21:00:46 +0700

    Add files via upload

---
+++ latihan-springboot/latihan-springboot/src/main/java/com/kuliah/latihanspringboot/Halaman2/LulusSeleksi.java
@@ -0,0 +1,21 @@
+package com.kuliah.latihanspringboot.Halaman2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@Controller
+@RequestMapping(value = "/api")
+@Slf4j
+
+public class LulusSeleksi {
+    @GetMapping(value = "/LulusSeleksi")
+    public String openLoginPage(HttpServletResponse httpServletResponse, HttpServletRequest request){
+        log.info("Selamat Anda berhasil lulus seleksi SNMPTN 2023");
+        return "LulusSeleksi";
+    }
+}
